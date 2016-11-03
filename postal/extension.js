514f361c13dc393da2f210126ed626993c408bf1
TheCmar7 <runner.cram@gmail.com>
2016-11-03 16:05:46 -0700

    made some changes to the preformance requirements making them more specific

--- postal/extension.js
+++ postal/extension.js
@@ -2,6 +2,13 @@
 // Import the module and reference it with the alias vscode in your code below
 let vscode = require('vscode');
 
+class WordCounter {
+    constructor( ){
+        StatusBarItem;
+    }
+
+ }
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
@@ -9,6 +16,8 @@ function activate(context) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "postal" is now active!');
+
+    let wordcount = new WordCounter
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
@@ -18,8 +27,10 @@ function activate(context) {
 
         // Display a message box to the user
         vscode.window.showInformationMessage('Sam is a silly muffin!');
+
     });
 
+    context.subscriptions.push(wordCounter);
     context.subscriptions.push(disposable);
 }
 exports.activate = activate;
