719bba71cce919df6860c0d1fad6a2a10fd976f0
Sam Lichlyter <slichlyter12@me.com>
2016-11-09 15:45:35 -0800

    extension shows current working directory, open files, and a list of the files in the open directory; also updates the status bar on start and finish. Changed Hello World command to Postal

--- postal/extension.js
+++ postal/extension.js
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 let vscode = require('vscode');
+let fs = require('fs');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -17,42 +18,34 @@ function activate(context) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function() {
+    let FindFiles = vscode.commands.registerCommand('extension.findFiles', function() {
         // The code you place here will be executed every time your command is executed
 
         // Display a message box to the user
-        vscode.window.showInformationMessage('Sam is a silly muffin!');
-        //console.log('This is how the extension do '.concat(intCounter));
-        intCounter = intCounter + 1;
+        //vscode.window.showInformationMessage('Running Postal...');
+        vscode.window.setStatusBarMessage("Running Postal...");
 
         // Thenable<Uri[]> What is a thenable<uri[]>?
         //     -> https://code.visualstudio.com/Docs/extensionAPI/vscode-api#Thenable
         //        A thenable that resolves to an array of resource identifiers.
-        var files;
-        var filesThen;
-        var filesArray = [];
-        files = vscode.workspace.findFiles('*', 'hi');
-
-        // Found a website that gave me this https://spring.io/understanding/javascript-promises
-        // Thenables/Promises run asynchronously, therefore this finishes AFTER the following 
-        // print statements of filesArray and theFilesArray
-        var theFilesArray = files.then(function(gettingTheFiles) {
-            gettingTheFiles.forEach(function(fileInfo) {
-                //console.log(typeof(fileInfo.path));
-                console.log(fileInfo.path);
-                filesArray.push(fileInfo.path);
-            });
-            return (gettingTheFiles);
+        var files = vscode.workspace.findFiles('*', 'hi');
+        files.then(function(foundFiles) {
+            console.log("foundFiles");
+            console.log(foundFiles);
         });
 
-        console.log("theFilesArray:");
-        console.log(theFilesArray);
-        console.log("filesArray:");
-        console.log(filesArray);
+        var cwd = vscode.workspace.rootPath;
+        console.log("cwd:");
+        console.log(cwd);
+
+        var textDocuments = vscode.workspace.textDocuments;
+        console.log("textDocuments:");
+        console.log(textDocuments);
         
+        vscode.window.setStatusBarMessage("");
     });
 
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(FindFiles);
 }
 exports.activate = activate;
 
