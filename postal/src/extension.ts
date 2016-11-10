259b7360ed0390262b2c387670bc48d8e343ed5d
Sam Lichlyter <slichlyter12@me.com>
2016-11-10 14:29:59 -0800

    changed all the hello world syntax to now reflect postal, ie the command is now Postal instead of Hello World

--- postal/src/extension.ts
+++ postal/src/extension.ts
@@ -35,7 +35,7 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    let disposable = vscode.commands.registerCommand('extension.postal', () => {
         // The code you place here will be executed every time your command is executed
 
         // generate html list of files
