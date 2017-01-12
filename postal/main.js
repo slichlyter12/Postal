71afb9a38966453dc4da068759af6406549e90c0
Winkleer <winkleer@onid.oregonstate.edu>
2017-01-11 16:04:32 -0800

    electron working within VSC env

--- postal/main.js
+++ postal/main.js
@@ -8,17 +8,13 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({width: 1800, height: 1000})
 
   // and load the index.html of the app.
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+  win.loadURL('file://' + __dirname + '/index.html')
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  //win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
