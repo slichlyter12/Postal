b7b0e11db3475c168b8725e32d29876665c03e7d
Winkleer <winkleer@onid.oregonstate.edu>
2017-01-31 16:00:36 -0800

    refactored electron infastucture

--- postal/lib/app/main.js
+++ postal/lib/app/main.js
@@ -1,36 +1,36 @@
-console.log("first");
+const electron = require('electron')
+// Module to control application life.
+const app = electron.app
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow
 
-const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
 
-console.log("starting");
-
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win
+let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 1800, height: 1000})
-
-  console.log("creating window");
+  mainWindow = new BrowserWindow({width: 1800, height: 1000})
 
   // and load the index.html of the app.
-  win.loadURL('file://' + __dirname + '/index.html')
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'FileMap.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
 
   // Open the DevTools.
-  //win.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  win.on('closed', () => {
-
-    console.log("closed");
-
+  mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    win = null
+    mainWindow = null
   })
 }
 
@@ -40,26 +40,21 @@ function createWindow () {
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-
-  console.log("closing all windows");
-
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (win === null) {
+  if (mainWindow === null) {
     createWindow()
   }
 })
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
-
-console.log("created window");
