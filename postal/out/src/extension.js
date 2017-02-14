0c7da78ee792cafbd642a55f4d9373f188bbacff
Sam Lichlyter <slichlyter12@me.com>
2017-02-14 15:58:52 -0800

    added out dir

--- postal/out/src/extension.js
+++ postal/out/src/extension.js
@@ -10,69 +10,20 @@ var fs = require('file-system');
 var nodefs = require('fs');
 var cwd = require('cwd');
 var childProcess = require('child_process');
-var electronp = require('electron-prebuilt');
+var electronp = require('electron');
 var isWin = /^win/.test(process.platform);
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
-    // get filepath and create file
-    var htmlFilePath = vscode.workspace.rootPath;
-    htmlFilePath += "/dirStructure.html";
-    fs.writeFile(htmlFilePath, "\n", function (error) {
-        if (error) {
-            console.error("Error creating dirStructure.html");
-        }
-    });
-    let previewUri = vscode.Uri.parse("Files://" + htmlFilePath);
     let provider = new ContentProv_1.ContentProv();
     let registration = vscode.workspace.registerTextDocumentContentProvider('Files', provider);
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.postal', () => {
-        // The code you place here will be executed every time your command is executed
-        // generate html list of files
-        var files = vscode.workspace.findFiles('*', '');
-        files.then(function (foundFiles) {
-            // start/create file
-            var htmlOpen = "<!DOCTYPE html><html><head><title>Files</title></head><body><ul>\n";
-            fs.writeFile(htmlFilePath, htmlOpen, function (error) {
-                if (error) {
-                    console.error("Failed to write opening HTML\nerror: " + error);
-                }
-            });
-            // add file names to list
-            foundFiles.forEach(function (file) {
-                if (file.path != htmlFilePath) {
-                    let html = "<li>" + file + "</li>\n";
-                    fs.appendFile(htmlFilePath, html, function (error) {
-                        if (error) {
-                            console.error("Failed to write " + file + ", html: " + html + "\nerror: " + error);
-                        }
-                    });
-                }
-            });
-            // end file
-            var htmlClose = "</ul></body></html>\n";
-            fs.appendFile(htmlFilePath, htmlClose, function (error) {
-                if (error) {
-                    console.error("Failed to write closing HTML\nerror: " + error);
-                }
-            });
-            return vscode.commands.executeCommand('vscode.previewHtml', previewUri, vscode.ViewColumn.Two);
-        });
-    });
-    context.subscriptions.push(disposable);
     let parse = vscode.commands.registerCommand('extension.parse', () => {
         // GET GRAMMARS
         var grammarsFile = nodefs.readFileSync(__dirname + "/../../src/grammars.json", "utf8");
         var grammars = JSON.parse(grammarsFile);
-        // GET ALL FILES:
-        var allFiles = vscode.workspace.findFiles('*', '');
-        allFiles.then(function (foundFiles) {
-            //DO STUFF WITH ALL FILES
-            //console.log(JSON.stringify(allFiles));
-        });
         //GET FILES TO PARSE
         var files = [];
         for (var i = 0; i < grammars.grammars.length; i++) {
@@ -80,7 +31,7 @@ function activate(context) {
                 files.push(vscode.workspace.findFiles("*" + grammars.grammars[i].filetypes[j], ''));
             }
         }
-        var nameHolder = [];
+        var pathHolder = [];
         var linkHolder = [];
         var foundLinks = [];
         function onlyUnique(value, index, self) {
@@ -95,6 +46,7 @@ function activate(context) {
                             if (grammars.grammars[i].regex[k].hasOwnProperty(key)) {
                                 //console.log(grammars.grammars[i].regex[k][key] + " -> " + key);
                                 for (var b = 0; b < foundFiles.length; b++) {
+                                    // get regular expression and run it
                                     var regexString = grammars.grammars[i].regex[k][key];
                                     var regex = new RegExp(regexString, 'g');
                                     var content;
@@ -105,16 +57,18 @@ function activate(context) {
                                         content = nodefs.readFileSync(foundFiles[b].path, 'utf8');
                                     }
                                     var found = content.match(regex);
-                                    nameHolder.push(foundFiles[b].path.slice(foundFiles[b].path.lastIndexOf("/") + 1));
-                                    for (var c = 0; c < foundFiles.length; c++) {
-                                        if (found != null) {
-                                            foundLinks.push(c);
-                                        }
-                                    }
+                                    //Store array of all paths of files in current directory
+                                    pathHolder.push(foundFiles[b].path);
+                                    /* for(var c = 0; c < foundFiles.length; c++){
+                                         if(found != null){
+                                             foundLinks.push(c)
+                                         }
+                                     }
+                                     */
                                     if (found != null) {
                                         //linkHolder.push(foundFiles[b].path.slice(foundFiles[b].path.lastIndexOf("/")+1));
-                                        console.log(JSON.stringify(foundLinks));
-                                        linkHolder.push(foundLinks);
+                                        //console.log(JSON.stringify(foundLinks));
+                                        linkHolder.push(found);
                                     }
                                     else {
                                         linkHolder.push([]);
@@ -125,35 +79,102 @@ function activate(context) {
                         }
                     }
                 }
-                console.log(JSON.stringify(linkHolder));
-                //Writing to DataStruct.json
-                var nameHolderUnique = nameHolder.filter(onlyUnique);
+                //Setting up DataStruct data
+                var pathHolderU = pathHolder.filter(onlyUnique); //remove duplicate path names from when parser ran multiple times
+                //Get NAMES from end of path string
+                var dirHolder = []; //This will receive all folders in cwd
+                var nameHolder = []; //All names from end of pathHolderU
+                var hrefNameHolder = []; //Contains all found href link names that aren't in cwd
+                pathHolderU.forEach(function (path, index) {
+                    nameHolder.push(pathHolderU[index].slice(pathHolderU[index].lastIndexOf("/") + 1));
+                });
+                var allNamesHolder = dirHolder.concat(nameHolder, hrefNameHolder); //Combine directory, file, and discovered href names
+                console.log(JSON.stringify(allNamesHolder));
+                //Get LINKS
+                linkHolder = linkHolder.slice(0, nameHolder.length);
+                var catLinkHolder = []; //Same as link Holder but each internal string is reduced to filename and extension
+                var idLinkHolder = []; //Contains ids of catLinkHolder
+                var fakeDirLinkHolder = [];
+                var fakeHrefLinkHolder = [];
+                for (i = 0; i < linkHolder.length; i++) {
+                    for (j = 0; j < linkHolder[i].length; j++) {
+                        catLinkHolder.push([]);
+                        idLinkHolder.push([]);
+                    }
+                }
+                //1 Too many empty arrays
+                catLinkHolder.shift();
+                idLinkHolder.shift();
+                for (i = 0; i < linkHolder.length; i++) {
+                    for (j = 0; j < linkHolder[i].length; j++) {
+                        if (linkHolder[i] != null) {
+                            linkHolder[i][j] = linkHolder[i][j].slice(linkHolder[i][j].indexOf("\"") + 1, linkHolder[i][j].lastIndexOf(".") + 4);
+                            catLinkHolder[i] = linkHolder[i].filter(onlyUnique);
+                            if (nameHolder.includes(catLinkHolder[i][j])) {
+                                idLinkHolder[i][j] = nameHolder.indexOf(catLinkHolder[i][j]);
+                            }
+                            else {
+                                //Create Node Here
+                                idLinkHolder[i].shift(); //WROOOONG
+                            }
+                        }
+                        else {
+                            catLinkHolder[i].push([]);
+                        }
+                    }
+                }
+                dirHolder.forEach(function (name, index) {
+                    fakeDirLinkHolder.push([]);
+                });
+                hrefNameHolder.forEach(function (name, index) {
+                    fakeHrefLinkHolder.push([]);
+                });
+                //idLinkHolder.slice(0, nameHolder.length);
+                var allLinksHolder = fakeDirLinkHolder.concat(idLinkHolder, fakeHrefLinkHolder);
+                console.log(JSON.stringify(catLinkHolder));
+                console.log(JSON.stringify(allLinksHolder));
+                //Get IDS from the list of allNamesHolder
+                var idHolder = [];
+                allNamesHolder.forEach(function (name, index) {
+                    idHolder.push(index);
+                });
+                //console.log(JSON.stringify(idHolder));
+                //Get LEVELS from pathHolderU and allNamesHolder
+                var dirLevelsHolder = [];
+                var nameLevelsHolder = [];
+                var hrefLevelsHolder = [];
+                var topSlashCounter = (pathHolderU[0].match(/\//g) || []).length; //Finds the number of backslashes in the path of the first files, ie number of slashes in top directory
+                /*****************************/
+                ////////CHANGE WHEN DIRECTORIES ARE WORKING. Can't assume names are top level, have to compare names and directories for which has fewest slashes
+                /*****************************/
+                pathHolderU.forEach(function (path, index) {
+                    var currSlashCounter = (pathHolderU[index].match(/\//g) || []).length; //If same as topSlashCounter, at level 0
+                    nameLevelsHolder.push(currSlashCounter - topSlashCounter);
+                });
+                var allLevelsHolder = dirLevelsHolder.concat(nameLevelsHolder, hrefLevelsHolder); //Combine directory, file, and discovered href levels
+                //console.log(JSON.stringify(allLevelsHolder));
+                //Get TYPES from allNamesHolder
+                var typeHolder = [];
+                allNamesHolder.forEach(function (name, index) {
+                    typeHolder.push(name.slice(name.lastIndexOf(".") + 1));
+                });
+                //console.log(JSON.stringify(typeHolder));
                 var jsonHolder = {};
                 var FileData = {};
                 var FileStructs = [];
                 var ErrorStructs = [];
                 var matchedLinks = [];
-                for (var x = 0; x < nameHolderUnique.length; x++) {
-                    for (var y = 0; y < linkHolder.length; y++) {
-                        if (nameHolderUnique[x] == linkHolder[y]) {
-                            matchedLinks[x] = linkHolder[y + 1];
-                            /*if(matchedLinks[x].includes(nameHolderUnique[x])){
-                                matchedLinks[x].push({FileStructsid: x})
-                            }*/
-                            y++;
-                        }
-                    }
-                    //console.log(JSON.stringify(matchedLinks));
+                idHolder.forEach(function (id, index) {
                     FileStructs.push({
-                        id: x,
-                        level: 0,
-                        name: nameHolderUnique[x],
-                        type: nameHolderUnique[x].slice(nameHolderUnique[x].lastIndexOf(".") + 1),
-                        links: linkHolder[x],
+                        id: idHolder[index],
+                        level: allLevelsHolder[index],
+                        name: allNamesHolder[index],
+                        type: typeHolder[index],
+                        links: allLinksHolder[index],
                         errors: []
                     });
                     ErrorStructs.push({});
-                }
+                });
                 FileData = { FileStructs, ErrorStructs };
                 jsonHolder = JSON.stringify({ FileData });
                 nodefs.writeFileSync(__dirname + "/../../postal.json", jsonHolder, 'utf8');
@@ -170,20 +191,14 @@ function activate(context) {
         else {
             filePath = `${__dirname}/../../`;
         }
-        //console.log(filePath);
-        //var p = childProcess.spawn(electronp, [filePath + 'main.js']);
-        //console.log(p);
-        //console.log("children");
-        // exec('electron main.js', (error, stdout, stderr) => {
-        //     if (error) {
-        //         console.error(`exec error: ${error}`);
-        //         return;
-        //     }
-        //     console.log(`stdout: ${stdout}`);
-        //     console.log(`stderr: ${stderr}`);
-        // });
         try {
-            var command = './node_modules/.bin/electron.cmd';
+            var command;
+            if (isWin) {
+                command = './node_modules/.bin/electron.cmd';
+            }
+            else {
+                command = './node_modules/.bin/electron';
+            }
             var cwd = path.join(__dirname, '../../lib/app');
             command = command.replace(/\//g, path.sep);
             cwd = cwd.replace(/\//g, path.sep);
