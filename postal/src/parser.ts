80acf94ea36f5688e657631b385374a4571efbda
Sam Lichlyter <slichlyter12@me.com>
2017-02-18 15:12:02 -0800

    pulling out rules from grammar

--- postal/src/parser.ts
+++ postal/src/parser.ts
@@ -24,10 +24,63 @@ export class Parser {
         // get filetype
         var filetype = this.getFiletype(filepath);
 
-        
+        // get grammars
+        var grammars = this.getGrammars(filetype);
     }
 
     private getFiletype(filepath: string): string {
         return filepath.slice(filepath.lastIndexOf(".") + 1);
+    }
+
+    private buildRegex(rules: any): any {
+
+        var regex = [];
+        for (var i = 0; i < rules.length; i++) {
+            switch (rules[i].type) {
+                case "link":
+                    regex.push(rules[i].options.link)
+                    break;
+                case "tagged":
+
+            }
+        }
+
+        return regex;
+    }
+
+    private getGrammars(filetype: string): any {
+
+        // get entire grammars file
+        var grammarsFile = nodefs.readFileSync(__dirname + "/../../src/grammars.json", "utf8");
+        var grammars = JSON.parse(grammarsFile);
+
+        // pull out grammar rules for our filetype
+        var rules = [];
+        for (var i = 0; i < grammars.grammars.length; i++) {
+            for (var j = 0; j < grammars.grammars[i].filetypes.length; j++) {
+                if (grammars.grammars[i].filetypes[j] == filetype) {
+                    for (var k = 0; k < grammars.grammars[i].rules.length; k++) {
+                        var rule = grammars.grammars[i].rules[k];
+                        switch (rule.type) {
+                            case "link": 
+                                rule.regex = rule.options.link; 
+                                break;
+                            case "tagged":
+                                
+                                //rule.regex = 
+                                break;
+                        }
+                        rules.push(rule);
+                    }
+                }
+            }
+        }
+
+        // build regex from user
+        for (var i = 0; i < rules.length; i++) {
+
+        }
+
+        return rules;
     }
 }
