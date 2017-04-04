95349fc510111e5cab7b5672ed14dfc2840cacae
TheCmar7 <runner.cram@gmail.com>
2017-04-03 21:40:47 -0700

    fixed the issue where there ended up being like 20 electron process still open after closing the original

--- postal/lib/app/main.js
+++ postal/lib/app/main.js
@@ -1,10 +1,9 @@
-
 'use strict';
 
 const electron = require('electron')
-// Module to control application life.
+    // Module to control application life.
 const app = electron.app
-// Module to create native browser window.
+    // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
 const path = require('path')
@@ -16,30 +15,28 @@ const ipc = electron.ipcMain;
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow(
-    {
-      width: 1200, 
-      height: 800,
-      frame: false,
-      resizable: false
-    }
-  )// and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'FileMap.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
-  //mainWindow.openDevTools();
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+            width: 1200,
+            height: 800,
+            frame: false,
+            resizable: false
+        }) // and load the index.html of the app.
+    mainWindow.loadURL(url.format({
+            pathname: path.join(__dirname, 'FileMap.html'),
+            protocol: 'file:',
+            slashes: true
+        }))
+        //mainWindow.openDevTools();
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null
+    })
 
 }
 
@@ -48,24 +45,22 @@ function createWindow () {
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+app.on('window-all-closed', app.quit); //function () {
+// On OS X it is common for applications and their menu bar
+// to stay active until the user quits explicitly with Cmd + Q
+// if (process.platform !== 'darwin') {
+//   app.on('window-all-closed', app.quit);
+// }
+//})
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
+app.on('activate', function() {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow()
+    }
 })
 
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
-
-
